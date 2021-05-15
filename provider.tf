af605f721b54e2f026732987c82d066505c7fb98
akash <aahuja888@gmail.com>
2021-05-15 06:03:01 +0530

    Initial Commit

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm"{
+client_id= "8989c843-ec15-404d-a125-b14adb25137e"
+tenant_id= "1a65fdbe-f157-456f-8f74-356197313783"
+client_secret= "64kz9uK-_.afKy33Sm-64c-G3peDCeotIU"
+subscription_id= "7bb780d0-66eb-47a9-be48-c808127ef469"
+
+features{}
+}
